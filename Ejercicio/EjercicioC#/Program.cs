1b42f16235143b04cdd649be299b1c5285b58d82
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-05-17 16:59:47 -0300

    Versión 2 de la calculadora, falta terminar

--- Ejercicio/EjercicioC#/Program.cs
+++ Ejercicio/EjercicioC#/Program.cs
@@ -1,24 +1,61 @@
-﻿Console.WriteLine("1) Programa para invertir un número de N cifras:");
+﻿
+Console.WriteLine("2) Programa para la Calculadora V1: ");
 
-Console.WriteLine("Ingrese el número a invertir:");
+int seguirOperando = 1, numOperacion = 0;
+double num1, num2;
 
-int num = Convert.ToInt32(Console.ReadLine());
-// Se lee el numero ingresado por el usuario
+do
+{
+    Console.WriteLine("¿Qué operación desea realizar?");
 
-int numReverso = 0;
-int ultimoDigito = 0;
+    do
+    {
+        Console.WriteLine("5) Calcular valor absoluto \n6) Calcular cuadrado del número \n7) Calcular raíz cuadrada \n8) Calcular seno del número \n 9) Calcular coseno \n10) Obtener parte entera de un número en punto flotante ");
+        numOperacion = Convert.ToInt32(Console.ReadLine());
+        
+    } while (numOperacion < 5 || numOperacion > 10);
 
-while(num > 0 && num != 0){
+    Console.WriteLine("Ingrese el número a operar: ");
 
-    ultimoDigito = num % 10;
-    // Obtengo el último digito del numero
+    Console.WriteLine("Numero 1:");
+    num1 = Convert.ToDouble(Console.ReadLine());
 
-    numReverso = numReverso * 10 + ultimoDigito;
-    // En el número reverso voy guardando los números cada vez multiplicando por 10 para luego sumar el último digito a añadir 
+    if(numOperacion == 7){
+        while(num1 < 0){
+            Console.WriteLine("No se puede calcular la raíz cuadrada de un número negativo. Ingrese un número no negativo:");
+            num1 = Convert.ToDouble(Console.ReadLine());
 
-    num = num/10;
-    // Divido el numero original en 10 de manera sucesiva
+        }
+    }
 
-}
+    switch (numOperacion)
+    {
+        case 5:
+            Console.WriteLine($"El valor absoluto es: {Math.Abs(num1)}");
+            break;
+        case 6:
+            Console.WriteLine($"El cuadrado del número es: {Math.Pow(num1,2)}");
+            break;
+        case 7:
+            Console.WriteLine($"La raíz cuadrada del número es: {Math.Sqrt(num1)}");
+            break;
+        case 8:
+            Console.WriteLine($"El seno del número es: {Math.Sin(num1)}");
+            break;
+        case 9:
+            Console.WriteLine($"El coseno del número es: {Math.Cos(num1)}");
+            break;
+        case 10:
+            Console.WriteLine($"La parte entera del float es: {Math.Truncate(num1)}");
+            break;
+    }
 
-Console.WriteLine($"El número reverso es {numReverso}. Si ingresó un número menor o igual a cero se muestra como resultado 0.");
+    do
+    {
+        Console.WriteLine("¿Desea seguir usando la calculadora? (1: sí, 0: no)");
+        seguirOperando = Convert.ToInt32(Console.ReadLine());
+
+    } while (seguirOperando < 0 || seguirOperando > 1);
+
+} while (seguirOperando == 1);
+
