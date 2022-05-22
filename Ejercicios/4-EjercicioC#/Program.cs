b94d07f134e9ee9dff181d19325776113ebfc4de
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-05-22 18:34:59 -0300

    Trabajo con strings

--- Ejercicios/4-EjercicioC#/Program.cs
+++ Ejercicios/4-EjercicioC#/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("4) Programa trabajando con el tipo string:");
+
+Console.WriteLine("Ingrese una cadena: ");
+string cadena1 = Console.ReadLine();
+
+// CONSULTA: ¿A qué se refiere con obtener algunas letras que forman parte de la cadena?
+
+// Obtener la longitud de la cadena:
+
+int longitudCadena = cadena1.Length;
+
+Console.WriteLine($"La longitud de la cadena es {longitudCadena}");
+
+Console.WriteLine("Ingrese otra cadena: ");
+string cadena2 = Console.ReadLine();
+
+// Concatenar cadenas
+string cadenaConcatenada = cadena1+cadena2;
+
+Console.WriteLine("La cadena concatenada es: ");
+Console.WriteLine(cadenaConcatenada);
+
+// Obtengo una subcadena de la cadenaConcatenada
+
+string subcadena = cadenaConcatenada.Substring(longitudCadena);
+// Se obtendrá en realidad la segunda cadena.
+
+Console.WriteLine("Obtengo una subcadena de la cadena concatenada: ");
+Console.WriteLine(subcadena);
